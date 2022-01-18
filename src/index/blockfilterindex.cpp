f0108ea68fdc247ceffe331e863713f9c5083de7
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2022-01-17 20:35:02 -0500

    indexes, refactor: Remove CChainState use in index CommitInternal method
    Replace CommitInternal method with CustomCommit and use interfaces::Chain
    instead of CChainState to generate block locator.
    
    This commit does not change behavior in any way, except in the
    (m_best_block_index == nullptr) case, which was added recently in
    https://github.com/bitcoin/bitcoin/pull/24117 as part of an ongoing attempt to
    prevent index corruption if bitcoind is interrupted during startup. New
    behavior in that case should be slightly better than the old behavior (skipping
    the entire custom+base commit now vs only skipping the base commit previously)
    and this might avoid more cases of corruption.

--- src/index/blockfilterindex.cpp
+++ src/index/blockfilterindex.cpp
@@ -157,7 +157,7 @@ bool BlockFilterIndex::CustomInit(const std::optional<interfaces::BlockKey>& blo
     return true;
 }
 
-bool BlockFilterIndex::CommitInternal(CDBBatch& batch)
+bool BlockFilterIndex::CustomCommit(CDBBatch& batch)
 {
     const FlatFilePos& pos = m_next_filter_pos;
 
@@ -176,7 +176,7 @@ bool BlockFilterIndex::CommitInternal(CDBBatch& batch)
     }
 
     batch.Write(DB_FILTER_POS, pos);
-    return BaseIndex::CommitInternal(batch);
+    return true;
 }
 
 bool BlockFilterIndex::ReadFilterFromDisk(const FlatFilePos& pos, const uint256& hash, BlockFilter& filter) const
--- src/index/blockfilterindex.h
+++ src/index/blockfilterindex.h
@@ -47,7 +47,7 @@ private:
 protected:
     bool CustomInit(const std::optional<interfaces::BlockKey>& block) override;
 
-    bool CommitInternal(CDBBatch& batch) override;
+    bool CustomCommit(CDBBatch& batch) override;
 
     bool CustomAppend(const interfaces::BlockInfo& block) override;
 
