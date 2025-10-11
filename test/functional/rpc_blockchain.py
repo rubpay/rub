9b99f1f108e3b6149243e5999a08378fd8af9101
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-10-11 20:26:42 +0530

    merge bitcoin#24528: rename getdeploymentinfo status-next to status_next

--- test/functional/rpc_blockchain.py
+++ test/functional/rpc_blockchain.py
@@ -219,7 +219,7 @@ class BlockchainTest(BitcoinTestFramework):
                     'min_activation_height': 0,
                     'since': 0,
                     'status': 'defined',
-                    'status-next': 'defined',
+                    'status_next': 'defined',
                     'ehf': True,
                 },
                 'active': False
@@ -233,7 +233,7 @@ class BlockchainTest(BitcoinTestFramework):
                     'min_activation_height': 0,
                     'since': 144,
                     'status': 'started',
-                    'status-next': status_next,
+                    'status_next': status_next,
                     'statistics': {
                         'period': 144,
                         'threshold': 108,
