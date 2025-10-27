f9bb07ee92f29ccd232fa60857a27401f8ca3380
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2025-10-27 16:38:42 +0530

    refactor: force stricter lambda signature for `ForEachMN{,Shared}()`

--- src/coinjoin/client.cpp
+++ src/coinjoin/client.cpp
@@ -1048,9 +1048,8 @@ CDeterministicMNCPtr CCoinJoinClientManager::GetRandomNotUsedMasternode()
     // fill a vector
     std::vector<CDeterministicMNCPtr> vpMasternodesShuffled;
     vpMasternodesShuffled.reserve(nCountEnabled);
-    mnList.ForEachMNShared(true, [&vpMasternodesShuffled](const CDeterministicMNCPtr& dmn) {
-        vpMasternodesShuffled.emplace_back(dmn);
-    });
+    mnList.ForEachMNShared(/*onlyValid=*/true,
+                           [&vpMasternodesShuffled](const auto& dmn) { vpMasternodesShuffled.emplace_back(dmn); });
 
     // shuffle pointers
     Shuffle(vpMasternodesShuffled.begin(), vpMasternodesShuffled.end(), FastRandomContext());
